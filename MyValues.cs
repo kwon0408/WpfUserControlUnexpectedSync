cad4672a40fe87e95c02be20daf5dba2628f7254
J. Kwon <henry.kwon.0408@gmail.com>
2023-11-27 17:36:53 +0900

    초기 커밋.

---
+++ MyValues.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class MyValues: INotifyPropertyChanged
+    {
+        public MyValues()
+        {
+        }
+
+        private double x;
+        public double X
+        {
+            get => x;
+            set
+            {
+                x = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+
+        private double y;
+        public double Y
+        {
+            get => y;
+            set
+            {
+                y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
---
+++ ValueIndicator.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    public partial class ValueIndicator: UserControl
+    {
+        public MyValues? Values
+        {
+            get => GetValue(ValuesProperty) as MyValues;
+            set => SetValue(ValuesProperty, value);
+        }
+        public static readonly DependencyProperty ValuesProperty =
+              DependencyProperty.Register(
+                  nameof(Values),
+                  typeof(MyValues),
+                  typeof(ValueIndicator),
+                  new PropertyMetadata(new MyValues())
+                  );
+
+        public string? DisplayName
+        {
+            get => GetValue(DisplayNameProperty) as string;
+            set => SetValue(DisplayNameProperty, value);
+        }
+        public static readonly DependencyProperty DisplayNameProperty =
+              DependencyProperty.Register(
+                  nameof(DisplayName),
+                  typeof(string),
+                  typeof(ValueIndicator),
+                  new PropertyMetadata("X")
+                  );
+        
+        public bool IsGuidelineVisible
+        {
+            get => GetValue(IsGuidelineVisibleProperty) as bool? ?? false;
+            set => SetValue(IsGuidelineVisibleProperty, value);
+        }
+        public static readonly DependencyProperty IsGuidelineVisibleProperty =
+              DependencyProperty.Register(
+                  nameof(IsGuidelineVisible),
+                  typeof(bool),
+                  typeof(ValueIndicator),
+                  new PropertyMetadata(false)
+                  );
+
+        public ValueIndicator()
+        {
+            InitializeComponent();
+            Values = new();
+        }
+    }
+}
